046aa1dc189a4b57890b3211c074fcfe272e7f03
mofath <mohamed.elgaedi@gmail.com>
2021-02-06 10:57:49 +0200

    Fixed bug at [service] : create new serviceRegistry instance

--- server/service.js
+++ server/service.js
@@ -1,28 +1,31 @@
-const express = require("express");
+const express = require('express');
+const ServiceRegistry = require('./lib/ServiceRegistry');
 
 const service = express();
 
 module.exports = (config) => {
   const log = config.log();
+  const serviceRegistry = new ServiceRegistry(log);
   // Add a request logging middleware in development mode
-  if (service.get("env") === "development") {
+  if (service.get('env') === 'development') {
     service.use((req, res, next) => {
       log.debug(`${req.method}: ${req.url}`);
       return next();
     });
   }
 
-  service.put(
-    `/register/:servicename/:serviceversion/:serviceport`, (req, res, next) => {
-      return next(`Not implemented`);
+  service.put('/register/:servicename/:serviceversion/:serviceport', (req, res, next) => {
+    const { servicename, serviceversion, serviceport } = req.params;
+    const serviceip = req.connection.remoteAddress.includes('::') ? `[${req.connection.remoteAddress}]` : `${req.connection.remoteAddress}`;
+    const serviceKey = serviceRegistry.register(servicename, serviceversion, serviceport, serviceip);
+    return res.json({ result: serviceKey });
   });
 
-  service.delete(
-    `/register/:servicename/:serviceversion/:serviceport`, (req, res, next) => {
-      return next(`Not implemented`);
+  service.delete('/register/:servicename/:serviceversion/:serviceport', (req, res, next) => {
+    return next(`Not implemented`);
   });
 
-  service.find(`/find/:servicename/:serviceversion`, (req, res, next) => {
+  service.get('/find/:servicename/:serviceversion', (req, res, next) => {
     return next(`Not implemented`);
   });
 
