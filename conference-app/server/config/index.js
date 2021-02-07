a2cc0ac40471fb2f83cdce93bfda8ee25df86554
mofath <mohamed.elgaedi@gmail.com>
2021-02-08 01:10:42 +0200

    [conference-app] : Fixed bug; missed backslash at speaker service url

--- conference-app/server/config/index.js
+++ conference-app/server/config/index.js
@@ -1,12 +1,12 @@
 module.exports = {
   development: {
     sitename: 'Cairo Meetups [Development]',
-    serviceRegistryUrl: 'http:/localhost:3000',
+    serviceRegistryUrl: 'http://localhost:3000',
     serviceVersionIdentifier: '1.x.x',
   },
   production: {
     sitename: 'Cairo Meetups',
-    serviceRegistryUrl: 'http:/localhost:3000',
+    serviceRegistryUrl: 'http://localhost:3000',
     serviceVersionIdentifier: '1.x.x',
   },
 };
