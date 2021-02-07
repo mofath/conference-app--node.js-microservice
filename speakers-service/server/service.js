19bfc180ef7d46ed39711873938f3a91d2d3df52
mofath <mohamed.elgaedi@gmail.com>
2021-02-07 17:55:24 +0200

    [speakers-service] : implemented service routes

--- speakers-service/server/service.js
+++ speakers-service/server/service.js
@@ -2,45 +2,80 @@ const express = require("express");
 
 const service = express();
 
+const Speakers = require("./lib/Speakers");
+
 module.exports = (config) => {
   const log = config.log();
+
+  const speakers = new Speakers(config.data.speakers);
+
+  // Add a request logging middleware in development mode
   if (service.get("env") === "development") {
     service.use((req, res, next) => {
-      service.use((req, res, next) => {
-        log.debug(`${req.method} : ${req.url}`);
-        return next();
-      });
+      log.debug(`${req.method}: ${req.url}`);
+      return next();
     });
   }
-};
 
-service.get('/list', (req, res, next) =>{
-  return next('Not implemented');
-});
-
-service.get('/list-short', (req, res, next) =>{
-  return next('Not implemented');
-});
-
-service.get('/names', (req, res, next) =>{
-  return next('Not implemented');
-});
-
-service.get('/speaker/:shortname', (req, res, next) =>{
-  return next('Not implemented');
-});
-
-service.get('/artwork/:shortname', (req, res, next) =>{
-  return next('Not implemented');
-});
-
-
-service.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  log.error(error);
-  return res.json({
-    error: {
-      message: error.message,
-    },
+  service.get("/list", async (req, res, next) => {
+    try {
+      return res.json(await speakers.getList());
+    } catch (err) {
+      return next(err);
+    }
   });
-});
+
+  service.get("/list-short", async (req, res, next) => {
+    try {
+      return res.json(await speakers.getListShort());
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+  service.get("/names", async (req, res, next) => {
+    try {
+      return res.json(await speakers.getNames());
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+  service.get("/artwork", async (req, res, next) => {
+    try {
+      return res.json(await speakers.getAllArtwork());
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+  service.get("/speaker/:shortname", async (req, res, next) => {
+    try {
+      return res.json(await speakers.getSpeaker(req.params.shortname));
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+  service.get("/artwork/:shortname", async (req, res, next) => {
+    try {
+      return res.json(
+        await speakers.getArtworkForSpeaker(req.params.shortname)
+      );
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+  service.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    log.error(error);
+    return res.json({
+      error: {
+        message: error.message,
+      },
+    });
+  });
+
+  return service;
+};
