4a5ca931e00e7c6534407132c43e89fd3627b22b
mofath <mohamed.elgaedi@gmail.com>
2021-02-07 19:36:03 +0200

    [conference-app] : Cleaning up the main app

--- conference-app/server/config/index.js
+++ conference-app/server/config/index.js
@@ -1,18 +1,12 @@
-const path = require('path');
-
 module.exports = {
   development: {
     sitename: 'Cairo Meetups [Development]',
-    data: {
-      speakers: path.join(__dirname, '../data/speakers.json'),
-      feedback: path.join(__dirname, '../data/feedback.json'),
-    },
+    serviceRegistryUrl: 'http:/localhost:3000',
+    serviceVersionIdentifier: '1.x.x',
   },
   production: {
     sitename: 'Cairo Meetups',
-    data: {
-      speakers: path.join(__dirname, '../data/speakers.json'),
-      feedback: path.join(__dirname, '../data/feedback.json'),
-    },
+    serviceRegistryUrl: 'http:/localhost:3000',
+    serviceVersionIdentifier: '1.x.x',
   },
 };
